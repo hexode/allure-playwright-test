06d9a78d60ea6b4fdb669858365276608d6fb598
Alexander Verkhoglyad <hexode@yandex-team.ru>
2022-05-13 19:13:45 +0300

    update

--- test/first.spec.ts
+++ test/first.spec.ts
@@ -7,31 +7,34 @@ const PO = {
   TopLevelTitle: '//h1'
 }
 
+const NonAutomatedStep = (stepName: string) => test.step(`${stepName}`, async () => ({}));
+const NonAutomatedCheck = (stepName: string) => test.step(`${stepName}`, async () => ({}));
 
 const Steps = {
-    OpenPortal: (page: Page) => test.step('[Arrange] Open portal', async () => {
+    OpenPortal: (page: Page) => test.step('Open portal (toloka.ai)', async () => {
       await page.goto('https://toloka.ai');
     }),
-    ClickGetFreeDemo: (page: Page) => test.step('[Act] Click get free demo', async () => {
+    ClickGetFreeDemo: (page: Page) => test.step('Click get free demo', async () => {
       await page.click(PO.GetFreeDemoLink);
     }),
-    AssertTopLevelTitle: (page: Page, title: string) => test.step(`[Assert] Check top level title is "${title}"`, async () => {
-      expect(await page.textContent(PO.TopLevelTitle)).toBe(title);
-    })
+};
+
+const Checks = {
+  AssertTopLevelTitle: (page: Page, title: string) => test.step(`Check top level title is "${title}"`, async () => {
+    expect(await page.textContent(PO.TopLevelTitle)).toBe(title);
+  })
 };
 
 
 
 test('basic automated test', async ({ page }) => {
+  allure.label({ name: "testId", value: "100"});
   allure.epic("Foo"); 
   allure.story("Story A");
-  allure.label({ name: "testId", value: "100"});
 
-
-  // arrange - подготовка
   await Steps.OpenPortal(page);
-  await Steps.ClickGetFreeDemo(page)
-  await Steps.AssertTopLevelTitle(page, 'Live demos')
+  await Steps.ClickGetFreeDemo(page);
+  await Checks.AssertTopLevelTitle(page, 'Live demos');
 });
 
 
@@ -40,16 +43,10 @@ test('basic test case', async ({ page }) => {
   allure.label({ name: "ALLURE_MANUAL", value: "true"});
   allure.epic("Foo"); 
   allure.story("Story B");
-  allure.id
-  // arrange - подготовка
+
   await Steps.OpenPortal(page);
-
-  await test.step('[Act] Click "Start now"', async() => {
-
-  });
-  await test.step('[Assert] Check top level title is "I want to"', async() => {
-
-  });
+  await NonAutomatedStep('Click "Start now"');
+  await NonAutomatedCheck('Check top level title is "I want to"');
 });
 
 
